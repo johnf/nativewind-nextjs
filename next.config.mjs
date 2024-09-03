c0e308693196f01a5461fe442741143950b86264
John Ferlito <johnf@inodes.org>
2024-09-03 16:20:39 +1000

    Add react-native-web

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  webpack: (config) => {
+    config.resolve.alias = {
+     ...(config.resolve.alias || {}),
+     'react-native$': 'react-native-web',
+    };
+
+    return config;
+  },
+};
 
 export default nextConfig;
